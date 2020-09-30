97043a65a7b6f37f72a238c8fe0b89af0abefd71
Hasan Ozovali <ozovalihasan@gmail.com>
2020-09-30 13:51:15 +0300

    Remove unnecessary parts and add rule for scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index'),
+  entry: path.resolve(__dirname, "src", "index"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
   },
-  resolve: {
-    extensions: ['.js', '.css', 'gif', 'jpg', 'svg', 'png'],
-  },
+
   module: {
     rules: [
       {
         test: /\.js/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
               [
-                '@babel/preset-env',
+                "@babel/preset-env",
                 {
                   targets: {
-                    node: 'current',
+                    node: "current",
                   },
                 },
               ],
@@ -34,11 +31,22 @@ module.exports = {
       },
       {
         test: /(\.css)$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
+        use: ["file-loader"],
       },
     ],
   },
