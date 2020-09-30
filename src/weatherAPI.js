c5adf246fc184d015a934840f45c275a8d737d03
Hasan Ozovali <ozovalihasan@gmail.com>
2020-09-30 16:44:24 +0300

    Update fetch mode and url

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,7 +1,13 @@
 const getInformation = async (city, unit = 'metric') => {
   const API_KEY = '735520cef897be860b5939b8127bf2ab';
   const resultComplex = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${API_KEY}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${
+      city
+    }&units=${
+      unit
+    }&appid=${
+      API_KEY}`,
+    { mode: 'cors' },
   );
   const result = await resultComplex.json();
   return result;
