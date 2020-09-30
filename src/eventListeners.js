aa44191f3d4273aca6ac8d78133d0c12b0cfdece
Hasan Ozovali <ozovalihasan@gmail.com>
2020-09-30 13:58:57 +0300

    Add parts to update unit and visibility of loader

--- src/eventListeners.js
+++ src/eventListeners.js
@@ -1,29 +1,37 @@
 // import storage from "./localStorageOperations";
 import getInformation from "./weatherAPI";
 import pageStructure from "./pageStructure";
+import storage from "./localStorageOperations";
 
 const eventListeners = () => {
+  const unit = document.getElementById(storage.getUnit());
+  unit.checked = true;
+
   const searchForm = () => document.getElementById("search");
   searchForm().addEventListener("submit", (event) => {
     event.preventDefault();
+    pageStructure.showLoader();
+
     const city = searchForm().elements.namedItem("city").value;
+    const unit = searchForm().elements.namedItem("unit").value;
+    storage.setUnit(unit);
     if (city === "") {
       alert("Please fill in the blank used for city");
     } else {
       (async () => {
         try {
-          const result = await getInformation(city);
+          const result = await getInformation(city, unit);
           if (result.cod === "404") {
-            console.log("City cannot be found ");
+            alert("City cannot be found ");
           } else {
-            console.log(result);
             pageStructure.showResults(result);
           }
         } catch (error) {
-          console.log(error);
+          alert(error);
         }
       })();
     }
+    pageStructure.removeLoader();
   });
 };
 
